8c57600d7506f35776b859f76b150c99f5a1b77e
Volker Bardenhorst <volker.bardenhorst@uniper.energy>
2022-04-29 16:45:20 +0200

    now two seperate services

--- AvgCurves.scala
+++ AvgCurves.scala
@@ -1,4 +1,5 @@
-val srv  = new Service("script", Some(dsPrd), Some(cdsDev)) with Containers with Markets with Lim2
+val srv  = new Service("prd", Some(dsPrd), Some(cdsPrd)) with Containers with Markets with Lim2
+val srv2  = new Service("uat", Some(dsUat), Some(cdsUat)) with Containers with Markets with Lim2
 
 val asof = yesterday
 
@@ -6,23 +7,15 @@ implicit val wb = FO_SUPPORT
 
 log info modelReturnMap(200, "AvgCurves started")
 
-try {
+val fo35FobAraBAvg = for {
+	foM0  <- srv.GoEndurCurves.Fo35FobAraBMonthlyAvgUsd get asof flatMap { _.series } map { s => Series(s.head) }
+	foFwd <- srv.OilEodFc.Fo35FobAraBarges get asof flatMap { _.series } map { _ from (foM0.start + 1) }
+} yield foM0 ++ foFwd
 
-	val fo35FobAraBAvg = for {
-		foM0  <- srv.GoEndurCurves.Fo35FobAraBMonthlyAvgUsd get asof flatMap { _.series } map { s => Series(s.head) }
-		foFwd <- srv.OilEodFc.Fo35FobAraBarges get asof flatMap { _.series } map { _ from (foM0.start + 1) }
-	} yield foM0 ++ foFwd
+val iceBrentAvg = for {
+	brentM0  <- srv.GoEndurCurves.IceBrentSwapMonthlyAvgUsd get asof flatMap { _.series } map { s => Series(s.head) }
+	brentFwd <- srv.OilEodFc.IceBrentSwap get asof flatMap { _.series } map { _ from (brentM0.start + 1) }
+} yield brentM0 ++ brentFwd
 
-	val iceBrentAvg = for {
-		brentM0  <- srv.GoEndurCurves.IceBrentSwapMonthlyAvgUsd get asof flatMap { _.series } map { s => Series(s.head) }
-		brentFwd <- srv.OilEodFc.IceBrentSwap get asof flatMap { _.series } map { _ from (brentM0.start + 1) }
-	} yield brentM0 ++ brentFwd
-
-	fo35FobAraBAvg map { srv.OilEodFc.Fo35FobAraBargesMonthlyAvgUsd.getOrCreate(asof) <-- _ }
-	iceBrentAvg map { srv.OilEodFc.IceBrentSwapMonthlyAvgUsd.getOrCreate(asof) <-- _ }
-
-} catch {
-
-	case throwable: Throwable =>
-		log error modelReturnMap(401, throwable.getMessage)
-}
+fo35FobAraBAvg map { srv2.OilEodFc.Fo35FobAraBargesMonthlyAvgUsd.getOrCreate(asof) <-- _ }
+iceBrentAvg map { srv2.OilEodFc.IceBrentSwapMonthlyAvgUsd.getOrCreate(asof) <-- _ }
