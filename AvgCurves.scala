1a9e5fc1e831f991a2e7d350f8b502e2c1c06b07
Volker Bardenhorst <volker.bardenhorst@uniper.energy>
2022-04-29 15:07:42 +0200

    added log statements

--- AvgCurves.scala
+++ AvgCurves.scala
@@ -4,15 +4,25 @@ val asof = yesterday
 
 implicit val wb = FO_SUPPORT
 
-val fo35FobAraBAvg = for {
-	foM0  <- srv.GoEndurCurves.Fo35FobAraBMonthlyAvgUsd get asof flatMap { _.series } map { s => Series(s.head) }
-	foFwd <- srv.OilEodFc.Fo35FobAraBarges get asof flatMap { _.series } map { _ from (foM0.start + 1) }
-} yield foM0 ++ foFwd
+log info modelReturnMap(200, "AvgCurves started")
 
-val iceBrentAvg = for {
-	brentM0  <- srv.GoEndurCurves.IceBrentSwapMonthlyAvgUsd get asof flatMap { _.series } map { s => Series(s.head) }
-	brentFwd <- srv.OilEodFc.IceBrentSwap get asof flatMap { _.series } map { _ from (brentM0.start + 1) }
-} yield brentM0 ++ brentFwd
+try {
 
-fo35FobAraBAvg map { srv.OilEodFc.Fo35FobAraBargesMonthlyAvgUsd.getOrCreate(asof) <-- _ }
-iceBrentAvg map { srv.OilEodFc.IceBrentSwapMonthlyAvgUsd.getOrCreate(asof) <-- _ }
+	val fo35FobAraBAvg = for {
+		foM0  <- srv.GoEndurCurves.Fo35FobAraBMonthlyAvgUsd get asof flatMap { _.series } map { s => Series(s.head) }
+		foFwd <- srv.OilEodFc.Fo35FobAraBarges get asof flatMap { _.series } map { _ from (foM0.start + 1) }
+	} yield foM0 ++ foFwd
+
+	val iceBrentAvg = for {
+		brentM0  <- srv.GoEndurCurves.IceBrentSwapMonthlyAvgUsd get asof flatMap { _.series } map { s => Series(s.head) }
+		brentFwd <- srv.OilEodFc.IceBrentSwap get asof flatMap { _.series } map { _ from (brentM0.start + 1) }
+	} yield brentM0 ++ brentFwd
+
+	fo35FobAraBAvg map { srv.OilEodFc.Fo35FobAraBargesMonthlyAvgUsd.getOrCreate(asof) <-- _ }
+	iceBrentAvg map { srv.OilEodFc.IceBrentSwapMonthlyAvgUsd.getOrCreate(asof) <-- _ }
+
+} catch {
+
+	case throwable: Throwable =>
+		log error modelReturnMap(401, throwable.getMessage)
+}
